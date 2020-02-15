f0be2db38f4fdc3cfd660f0be25629788d15c31d
User <User@DESKTOP-EGVOHB7>
2020-02-14 21:01:58 -0800

    Add project files.

---
+++ OnlineShopping/Controllers/PersonController.cs
@@ -0,0 +1,147 @@
+﻿using OnlineShopping.Models.DomainModel.DTO.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShopping.Controllers
+{
+    
+    public class PersonController : Controller
+    {
+        #region [- ctor -]
+        public PersonController()
+        {
+            Ref_PersonCrud = new Models.DomainModel.POCO.PersonCrud();
+        }
+        #endregion
+        #region [- props -]
+        public Models.DomainModel.POCO.PersonCrud Ref_PersonCrud { get; set; }
+        #endregion
+
+        #region [- Actions -]
+
+        #region [- Index() -]
+        // GET: Person
+        //[Route("index")]
+        public ActionResult Index()
+        {
+            return View(Ref_PersonCrud.Select());
+        }
+        #endregion
+
+        #region [- create -]
+        //[Route("Create")]
+        #region [- Get -]
+        // GET: People/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        #endregion
+
+        #region [- Post -]
+        // POST: People/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title,LastName,Age")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                Ref_PersonCrud.Insert(person);
+                return RedirectToAction("index");
+            }
+
+            return View(person);
+        }
+        #endregion
+        #endregion
+
+        #region [- Edit -]
+        #region [- Get -]
+        // GET: People/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var person = Ref_PersonCrud.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+        #endregion
+
+        #region [- Post -]
+        // POST: People/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title,LastName,Age")] Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                Ref_PersonCrud.Update(person);
+                return RedirectToAction("Index");
+            }
+            return View(person);
+        }
+        #endregion
+        #endregion
+
+        #region [- Delete -] 
+        #region [- Get -] 
+        // GET: People/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+           var person  = Ref_PersonCrud.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+        #endregion
+
+        #region [- Post -]
+        // POST: People/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Ref_PersonCrud.Delete(id);
+            return RedirectToAction("Index");
+        }
+        #endregion 
+        #endregion
+
+        // GET: People/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var person = Ref_PersonCrud.Find(id);
+            if (person == null)
+            {
+                return HttpNotFound();
+            }
+            return View(person);
+        }
+
+        #endregion
+    }
+}
---
+++ OnlineShopping/Models/DomainModel/POCO/PersonCrud.cs
@@ -0,0 +1,159 @@
+﻿using OnlineShopping.Models.DomainModel.DTO.EF;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShopping.Models.DomainModel.POCO
+{
+    public class PersonCrud
+    {
+        #region [- ctor -]
+        public PersonCrud()
+        {
+
+        }
+        #endregion
+
+        #region [- Tasks -] 
+
+        #region [- Select() -]
+        public List<Models.DomainModel.DTO.EF.Person> Select()
+        {
+            using (var context = new OnlineShoppingEntities())
+            {
+                try
+                {
+                    var q = context.Person.ToList();
+                    return q;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                finally
+                {
+                    if (context != null)
+                    {
+                        context.Dispose();
+                    }
+                }
+            }
+        }
+        #endregion 
+
+        #region [- Insert(Person ref_Person) -]
+        public void Insert(Person ref_Person)
+        {
+            using (var context = new OnlineShoppingEntities())
+            {
+                try
+                {
+                    context.Person.Add(ref_Person);
+                    context.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                finally
+                {
+                    if (context != null)
+                    {
+                        context.Dispose();
+                    }
+                }
+            }
+
+        }
+        #endregion
+
+        #region [- Find(int? id) -]
+        public Person Find(int? id)
+        {
+            using (var context = new OnlineShoppingEntities())
+            {
+                try
+                {
+                    Person person = context.Person.Find(id);
+                    return person;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                finally
+                {
+                    if (context != null)
+                    {
+                        context.Dispose();
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region [- Update(Person person) -]
+        public void Update(Person person)
+        {
+            using (var context = new OnlineShoppingEntities())
+            {
+                try
+                {
+                    context.Entry(person).State = EntityState.Modified;
+                    context.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                finally
+                {
+                    if (context != null)
+                    {
+                        context.Dispose();
+                    }
+                }
+            }
+
+        }
+        #endregion
+
+        #region [- Delete(int? id) -]
+        public void Delete(int id)
+        {
+            using (var context = new OnlineShoppingEntities())
+            {
+                try
+                {
+                    Person person = context.Person.Find(id);
+                    context.Person.Remove(person);
+                    context.SaveChanges();
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                finally
+                {
+                    if (context != null)
+                    {
+                        context.Dispose();
+                    }
+                }
+
+            }
+
+        } 
+        #endregion
+
+        #endregion
+    }
+}
